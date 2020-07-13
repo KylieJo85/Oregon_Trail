6526c896c335927d3d7dc9ee399d4b5264e8d473
Kylie Jo McCafferty <kyliemccafferty85@icloud.com>
2020-07-13 11:08:57 -0400

    Initial commit

---
+++ assets/CSS/JS/Travelers1.js
@@ -0,0 +1,29 @@
+//Thank you to Doug, Cynthia, Lesa, and Colin for reminding me to have two separate files for each class (travelers and wagons.)
+
+class travelers{
+    constructor(name){
+    this.name=name
+    this.food=1
+    this.isHealthy=true
+    }
+
+
+hunt() {
+    this.food+=2
+    
+    
+}
+
+eat() {
+    
+    if(this.food>0){
+       this.food-=1
+     
+    }
+    else{
+        this.isHealthy=false
+    }
+   
+
+}
+}
---
+++ assets/CSS/JS/Wagon1.js
@@ -0,0 +1,23 @@
+class wagon{
+    constructor(capacity){
+        this.capacity=capacity
+        this.passengers=[]
+    }
+    
+}
+
+getAvailableSeatCount()
+
+console.log()
+
+join()
+
+console.log()
+
+shouldQuarantine()
+.some
+console.log()
+
+totalFood()
+
+console.log()
